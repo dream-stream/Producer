96f36bad23a20387dedea92ff52c020d903f6fff
Anders Meidahl <Andersmeidahl@gmail.com>
2019-10-16 13:35:08 +0200

    Added Console writeline

--- Producer/TopicList.cs
+++ Producer/TopicList.cs
@@ -9,7 +9,9 @@ namespace Producer
         public const string TopicListPrefix = "TopicList/";
         public static async Task<int> GetPartitionCount(EtcdClient client, string topic)
         {
-            var partitionsString = await client.GetValAsync($"{TopicListPrefix}/{topic}");
+            var key = $"{TopicListPrefix}/{topic}";
+            Console.WriteLine($"Requesting Topic partition count for {key}");
+            var partitionsString = await client.GetValAsync(key);
             if (int.TryParse(partitionsString, out var partitions)) return partitions;
             throw new Exception("Topic does not exists");
         }
