693c4cddee1995485e36b6a289b821b5deedf853
Anders Meidahl <Andersmeidahl@gmail.com>
2019-10-16 13:24:22 +0200

    Corrected TopicListPrefix

--- Producer/TopicList.cs
+++ Producer/TopicList.cs
@@ -6,7 +6,7 @@ namespace Producer
 {
     public class TopicList
     {
-        public const string TopicListPrefix = "Topic/";
+        public const string TopicListPrefix = "TopicList/";
         public static async Task<int> GetPartitionCount(EtcdClient client, string topic)
         {
             var partitionsString = await client.GetValAsync($"{TopicListPrefix}/{topic}");
